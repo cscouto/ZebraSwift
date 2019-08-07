ca61d04f41966f18734e2d032dbc347a9555d44c
Tiago Do Couto <cs.couto@outlook.com>
2019-08-07 13:43:11 -0500

    Initial Commit

---
+++ ZebraSwift/ZebraSwift.h
@@ -0,0 +1,19 @@
+//
+//  ZebraSwift.h
+//  ZebraSwift
+//
+//  Created by Tiago Do Couto on 8/7/19.
+//  Copyright © 2019 Couto Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZebraSwift.
+FOUNDATION_EXPORT double ZebraSwiftVersionNumber;
+
+//! Project version string for ZebraSwift.
+FOUNDATION_EXPORT const unsigned char ZebraSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZebraSwift/PublicHeader.h>
+
+
